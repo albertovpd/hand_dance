f364f169fadfb0c1e1bdd515d11bcb22cf324c4a
albertovpd <a.vargas.pina@gmail.com>
2019-12-13 14:06:20 +0100

    finding some results

---
+++ src/multi.py
@@ -0,0 +1,7 @@
+# https://docs.python.org/3.5/library/multiprocessing.html#module-multiprocessing
+
+# https://stackify.com/when-to-use-asynchronous-programming/
+
+ # https://github.com/timofurrer/awesome-asyncio#alternatives-to-asyncio
+
+from multiprocessing import Pool
