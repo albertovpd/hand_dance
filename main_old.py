11b994058c58b58207882ef341c5823330da8760
albertovpd <a.vargas.pina@gmail.com>
2019-12-19 23:17:57 +0100

    project done but readme

--- main_old.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-#subprocess.run("python3 ./src/video_audio.py & python3 ./src/base_sound.py & python3 ./src/recording_environment.py", shell=True)    
-
-subprocess.run("python3 ./src/recording_environment.py", shell=True)    
-
